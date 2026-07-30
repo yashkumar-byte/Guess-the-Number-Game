a197e2ba2b4d3ffc4e52154099db83e39852eb0f
yashkumar-byte <yashkumar0925@gmail.com>
2026-07-30 23:40:04 +0530

    initially

--- GuessNumber.java
+++ GuessNumber.java
@@ -27,7 +27,7 @@ class Game{
     boolean isCorrectNumber(){
         noOfGuesses++;
         if(inputNumber==number){
-            System.out.format("jay ho you guessed it right, it was %d\nYou guessed it in %d attempts",number,noOfGuesses);
+            System.out.format("jio sher you guessed it right, it was %d\nYou guessed it in %d attempts",number,noOfGuesses);
             return true;
         }
         else if (inputNumber<number){
